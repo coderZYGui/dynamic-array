58a5556e5aeb947ece7dd0d1a4a28f9209f75509
zygui_g <zygui@163.com>
2020-03-15 20:08:47 +0800

    设计ArrayList的方法

---
+++ dynamic-array/src/com/zy/ArrayList.java
@@ -0,0 +1,99 @@
+package com.zy;
+
+/**
+ * Description: 设计动态数组
+ *
+ * @author zygui
+ * @date 2020/3/15 19:39
+ */
+public class ArrayList {
+    /****************************************/
+    // 方法暂时设计如下,后续会优化拓展
+    /****************************************/
+
+    /**
+     * 清除所有元素
+     */
+    public void clear(){
+
+    }
+
+    /**
+     * 元素的数量
+     * @return
+     */
+    public int size(){
+        return 0;
+    }
+
+    /**
+     * 是否为空
+     * @return
+     */
+    public boolean inEmpty(){
+        return false;
+    }
+
+    /**
+     * 是否包含某个元素
+     * @param element
+     * @return
+     */
+    public boolean contains(int element){
+        return false;
+    }
+
+    /**
+     * 添加元素到尾部
+     * @param element
+     */
+    public void add(int element){
+
+    }
+
+    /**
+     * 获取index位置的元素
+     * @param index
+     * @return
+     */
+    public int get(int index){
+        return 0;
+    }
+
+    /**
+     * s设置index位置的元素
+     * @param index
+     * @param element
+     * @return 原来的元素
+     */
+    public int set(int index, int element){
+        return 0;
+    }
+
+    /**
+     * 在index位置插入一个元素
+     * @param index
+     * @param element
+     */
+    public void add(int index, int element){
+
+    }
+
+    /**
+     * 删除index位置的元素
+     * @param index
+     * @return
+     */
+    public int remove(int index){
+        return 0;
+    }
+
+    /**
+     * 查看元素的索引
+     * @param element
+     * @return
+     */
+    public int indexOf(int element){
+        return -1;
+    }
+}
---
+++ dynamic-array/src/com/zy/Main.java
@@ -0,0 +1,13 @@
+package com.zy;
+
+/**
+ * Description:
+ *
+ * @author zygui
+ * @date 2020/3/15 19:39
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
