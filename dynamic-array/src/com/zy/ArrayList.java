778031376dfc636bdf7eee1e332d2bb10c7245f9
zygui_g <zygui@163.com>
2020-03-16 00:01:57 +0800

    对部分功能实现

--- dynamic-array/src/com/zy/ArrayList.java
+++ dynamic-array/src/com/zy/ArrayList.java
@@ -1,5 +1,6 @@
 package com.zy;
 
+
 /**
  * Description: 设计动态数组
  *
@@ -7,93 +8,178 @@ package com.zy;
  * @date 2020/3/15 19:39
  */
 public class ArrayList {
-    /****************************************/
-    // 方法暂时设计如下,后续会优化拓展
-    /****************************************/
+    /**
+     * 元素的数量
+     */
+    private int size;
+
+    /**
+     * 所有元素
+     */
+    private int[] elements;
+
+    /**
+     * 数组的默认容量
+     */
+    private static final int DEFAULT_CAPACITY = 10;
+
+    /**
+     * 找不到元素返回-1
+     */
+    private static final int ELEMENT_NOT_FOUNT = -1;
+
+    public ArrayList() {
+        // 默认容量
+        //elements = new int[DEFAULT_CAPACITY];
+        this(DEFAULT_CAPACITY); // 调用下面的构造器
+    }
+
+    public ArrayList(int capacity) {
+        // 设置默认容量为 10
+        capacity = (capacity < DEFAULT_CAPACITY) ? DEFAULT_CAPACITY : capacity;
+        elements = new int[capacity];
+    }
 
     /**
      * 清除所有元素
      */
-    public void clear(){
-
+    public void clear() {
+        size = 0;
     }
 
     /**
      * 元素的数量
+     *
      * @return
      */
-    public int size(){
-        return 0;
+    public int size() {
+        return size;
     }
 
     /**
      * 是否为空
+     *
      * @return
      */
-    public boolean inEmpty(){
-        return false;
+    public boolean inEmpty() {
+        return size == 0;
     }
 
     /**
      * 是否包含某个元素
+     *
      * @param element
      * @return
      */
-    public boolean contains(int element){
-        return false;
+    public boolean contains(int element) {
+        // 如果element元素可以找到
+        return indexOf(element) != ELEMENT_NOT_FOUNT;
     }
 
     /**
      * 添加元素到尾部
+     *
      * @param element
      */
-    public void add(int element){
-
+    public void add(int element) {
+        // elements[size] = element;
+        // size++;
+        elements[size++] = element;
     }
 
     /**
      * 获取index位置的元素
+     *
      * @param index
      * @return
      */
-    public int get(int index){
-        return 0;
+    public int get(int index) {
+        // 约束Index
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+        }
+        return elements[index];
     }
 
     /**
-     * s设置index位置的元素
+     * 设置index位置的元素
+     *
      * @param index
      * @param element
      * @return 原来的元素
      */
-    public int set(int index, int element){
-        return 0;
+    public int set(int index, int element) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+        }
+        int oldEle = elements[index];
+        elements[index] = element;
+        return oldEle;
     }
 
     /**
      * 在index位置插入一个元素
+     *
      * @param index
      * @param element
      */
-    public void add(int index, int element){
-
+    public void add(int index, int element) {
+        // 注意: 插入元素后,元素是从后开始往后挪
+        for (int i = size - 1; i >= index; i--) {
+            elements[i + 1] = elements[i];
+        }
+        elements[index] = element;
+        size++;
     }
 
     /**
      * 删除index位置的元素
+     *
      * @param index
-     * @return
+     * @return 被删除的元素
      */
-    public int remove(int index){
-        return 0;
+    public int remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index:" + index + ", Size:" + size);
+        }
+        int delEle = elements[index];
+
+        // 当删除一个元素时,需要挪动后面元素的范围
+        for (int i = index + 1; i <= size - 1; i++) {
+            elements[i - 1] = elements[i];
+        }
+        size--;
+        return delEle;
     }
 
     /**
      * 查看元素的索引
+     *
      * @param element
      * @return
      */
-    public int indexOf(int element){
-        return -1;
+    public int indexOf(int element) {
+        for (int i = 0; i < size; i++) {
+            if (elements[i] == element) {
+                return i;
+            }
+        }
+        return ELEMENT_NOT_FOUNT;
+    }
+
+    @Override
+    public String toString() {
+        // 打印格式: size=3, [10, 20, 30]
+        // 使用StringBuilder 效率高一些
+        StringBuilder string = new StringBuilder();
+        string.append("size=").append(size).append(", [");
+        for (int i = 0; i < size; i++) {
+            string.append(elements[i]);
+            if (i != size - 1) {
+                string.append(", ");
+            }
+        }
+        string.append("]");
+        return string.toString();
     }
 }
--- dynamic-array/src/com/zy/Main.java
+++ dynamic-array/src/com/zy/Main.java
@@ -8,6 +8,17 @@ package com.zy;
  */
 public class Main {
     public static void main(String[] args) {
+        ArrayList arrayList = new ArrayList();
+        arrayList.add(10);
+        arrayList.add(20);
+        arrayList.add(30);
+        arrayList.add(40);
+        arrayList.add(50);
 
+        arrayList.remove(1);
+
+        arrayList.add(0, 100);
+
+        System.out.println(arrayList);
     }
 }
